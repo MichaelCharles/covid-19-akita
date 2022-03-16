813f1d6c0fe6ad4869923464b22fd60444160b29
Michael Charles Aubrey <michael@akita-itlabo.com>
2022-03-16 15:28:35 +0900

    add meta data to index file

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import CovidLineChart from "./Chart";
 function App() {
   return (
     <div className="App">
-      <h1>
-      秋田県における新型コロナウイルス感染者の週平均推移
-      </h1>
+      <div className="content-wrapper">
+        <h1>秋田県における新型コロナウイルス感染者の週平均推移</h1>
+        <hr />
+        <p>
+          新型コロナウイルスの感染率について考える場合、ある日の症例数ではなく、全体の傾向について考えるのがよいでしょう。そのために、以下に1週間の平均患者数をピンク色で示したグラフを掲載する。また、1日の患者数はグレーで表示されています。
+        </p>
+        <hr />
+      </div>
       <CovidLineChart />
     </div>
   );
